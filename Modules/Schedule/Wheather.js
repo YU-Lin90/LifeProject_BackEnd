57030b7877df00e0d7dfd44b517e1bddd39975f8
YuChenLin <andy50152@gmail.com>
2022-12-22 12:43:30 +0800

    天氣預報 後端資料庫寫入完成

--- Modules/Schedule/Wheather.js
+++ Modules/Schedule/Wheather.js
@@ -2,16 +2,35 @@ const DB = require("../ConnectDataBase");
 const moment = require("moment-timezone");
 const nodeSchedule  = require('node-schedule')
 
-const updateTaskam6 = '* 1 6 * * *'
-const updateTaskpm0 = '* 1 12 * * *'
-const updateTaskpm6 = '* 1 18 * * *'
+const updateWeatherData = require('../StartCheck/Weather')
+
 //天氣資料更新預報排程
 //https://ithelp.ithome.com.tw/articles/10243928
 
 const setSchedule = async ()=>{
-  const testTask = '5 * * * * *'
-  const testJob = nodeSchedule.scheduleJob(testTask,()=>{
-    console.log('test' + new Date());
+  const updateWeatherDataTask1 = '0 30 0 * * *'
+  const updateWeatherDataTask2 = '0 30 6 * * *'
+  const updateWeatherDataTask3 = '0 42 12 * * *'
+  const updateWeatherDataTask4 = '0 30 18 * * *'
+
+  const updateWeatherData1 = nodeSchedule.scheduleJob(updateWeatherDataTask1,async ()=>{
+    await updateWeatherData()
+    console.log('updateWeatherAM0030');
+
+  })
+  const updateWeatherData2 = nodeSchedule.scheduleJob(updateWeatherDataTask2,async ()=>{
+    await updateWeatherData()
+    console.log('updateWeatherAM0630');
+
+  })
+  const updateWeatherData3 = nodeSchedule.scheduleJob(updateWeatherDataTask3,async ()=>{
+    await updateWeatherData()
+    console.log('updateWeatherPM1230');
+  })
+  const updateWeatherData4 = nodeSchedule.scheduleJob(updateWeatherDataTask4,async ()=>{
+    await updateWeatherData()
+    console.log('updateWeatherPM1830');
+
   })
 }
 
--- Modules/StartCheck/Weather.js
+++ Modules/StartCheck/Weather.js
@@ -1,28 +1,75 @@
 const DB = require("../../Modules/ConnectDataBase");
 const moment = require("moment-timezone");
-const axios = require('axios');
-const CwbKey = process.env.CWB_KEY
-const checkWeather = async ()=>{
-  const {data} = await axios.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${CwbKey}&format=JSON&sort=time&locationName=新北市`)
+const axios = require("axios");
+const CwbKey = process.env.CWB_KEY;
+const checkWeather = async () => {
+  const { data } = await axios.get(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${CwbKey}&format=JSON&sort=time`
+  );
   const datas = data.records.location;
-  for (let element of datas){
+  //各城市資料
+  for (let element of datas) {
     //locationName  縣市名稱
-    const {locationName,weatherElement} = element
+    const { locationName, weatherElement } = element;
     // console.log(locationName);
     // console.log(weatherElement);
-    const [{time:weather_condition}] = weatherElement.filter((v)=>{return v.elementName==='Wx'})
-    console.log({weather_condition});
-    const [{time:humidity}] = weatherElement.filter((v)=>{return v.elementName==='PoP'})
-    console.log({humidity});
-    const [{time:temp_min}] = weatherElement.filter((v)=>{return v.elementName==='MinT'})
-    console.log({temp_min});
-    const [{time:temp_max}] = weatherElement.filter((v)=>{return v.elementName==='MaxT'})
-    console.log({temp_max});
-    const [{time:feeling_condition}] = weatherElement.filter((v)=>{return v.elementName==='CI'})
-    console.log({feeling_condition});
+    //===============================================分隔線================================================
+    const [{ time: weather_condition }] = weatherElement.filter((v) => {
+      return v.elementName === "Wx";
+    });
+    // console.log({ weather_condition });
+    //===============================================分隔線================================================
+    const [{ time: precipitation }] = weatherElement.filter((v) => {
+      return v.elementName === "PoP";
+    });
+    // console.log({ precipitation });
+    //===============================================分隔線================================================
+    const [{ time: temp_min }] = weatherElement.filter((v) => {
+      return v.elementName === "MinT";
+    });
+    // console.log({ temp_min });
+    //===============================================分隔線================================================
+    const [{ time: temp_max }] = weatherElement.filter((v) => {
+      return v.elementName === "MaxT";
+    });
+    // console.log({ temp_max });
+    //===============================================分隔線================================================
+    const [{ time: feeling_condition }] = weatherElement.filter((v) => {
+      return v.elementName === "CI";
+    });
+    // console.log({ feeling_condition });
+    //===============================================分隔線================================================
+    const insertSql =
+    "INSERT INTO `weather_forecast`(`city_name`, `start_time`, `end_time`, `precipitation`, `weather_condition`, `temp_max`, `temp_min`, `feeling_condition`, `city_order`) VALUES (?,?,?,?,?,?,?,?,?)";
+    const updateSql = "UPDATE `weather_forecast` SET `start_time`=?,`end_time`=?,`precipitation`=?,`weather_condition`=?,`temp_max`=?,`temp_min`=?,`feeling_condition`=? WHERE `city_name` = ? AND `city_order` = ?"
+    for(let i = 0;i<3;i++){
+      const insertStartTime = weather_condition[i].startTime
+      const insertEndTime = weather_condition[i].endTime;
+      const insertPrecipitation = precipitation[i].parameter.parameterName
+      const insertWeather_condition = weather_condition[i].parameter.parameterName
+      const insertTemp_max = temp_max[i].parameter.parameterName
+      const insertTemp_min = temp_min[i].parameter.parameterName
+      const insertFeeling_condition = feeling_condition[i].parameter.parameterName
+      const insertCity_order = i + 1
+      const insertDatas = [insertStartTime,insertEndTime,insertPrecipitation,insertWeather_condition,insertTemp_max,insertTemp_min,insertFeeling_condition,locationName,insertCity_order]
+      const [result] = await DB.query(updateSql,insertDatas)
+      // console.log(result);
+    } 
 
-
+    /* 
+    {
+      "startTime": "2022-12-12 12:00:00",
+      "endTime": "2022-12-12 18:00:00",
+      "parameter": {
+          "parameterName": "稍有寒意"
+      }}
+  */
+      //這裡還在FOR迴圈內
+    //===============================================分隔線================================================
   }
-}
+  const upDateTimeSql = "UPDATE `weather_forecast` SET `start_time`=NOW() WHERE `city_order` = 0 AND `sid` = 1"
+  const [result] = await DB.query(upDateTimeSql)
+  console.log(result);
+};
 
 module.exports = checkWeather;
